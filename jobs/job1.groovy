ebfe81d94c89e11c4d89a28cdc2b0f7f35ba2697
Alex <georgievalexandro@gmail.com>
2022-02-08 13:52:01 +0200

    test2

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -1,7 +1,7 @@
 // def repos = ["https://github.com/georgievalexandro/learning-devops.git", "https://github.com/georgievalexandro/nda.git"]
 // def repos = readFileFromWorkspace("")
 for(i in 0..2) {
-    job("Scan repo-${i}") {
+    job("Repo${i}") {
         // scm {
         //     git(repos[i])
         // }
