ab6fa28ddf99578fc262afa6969647e4d0510fc6
Alex <georgievalexandro@gmail.com>
2022-02-08 13:15:35 +0200

    try reading file

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -1,12 +1,12 @@
-def repos = ["https://github.com/georgievalexandro/learning-devops.git", "https://github.com/georgievalexandro/nda.git"]
-
+// def repos = ["https://github.com/georgievalexandro/learning-devops.git", "https://github.com/georgievalexandro/nda.git"]
+def repos = readFileFromWorkspace('/jenkins_repos/github.txt')
 for(i in 0..2) {
     job("Scan repo-${i}") {
-        scm {
-            git(repos[i])
-        }
+        // scm {
+        //     git(repos[i])
+        // }
         steps {
-            shell("Hello Jenkins ${i}")
+            shell("cat repos")
         }
     }
 }
