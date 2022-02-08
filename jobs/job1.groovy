75bfb8dd12b3e41a289c7a9a0c592a1f9d6e18a5
Alex <georgievalexandro@gmail.com>
2022-02-08 13:41:10 +0200

    test

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -1,12 +1,12 @@
 // def repos = ["https://github.com/georgievalexandro/learning-devops.git", "https://github.com/georgievalexandro/nda.git"]
-def repos = readFileFromWorkspace('../jenkins_repos/github.txt')
+// def repos = readFileFromWorkspace("")
 for(i in 0..2) {
     job("Scan repo-${i}") {
         // scm {
         //     git(repos[i])
         // }
         steps {
-            shell("cat repos")
+            shell("cat /jenkins_repos/github.txt")
         }
     }
 }
