9a0b3c7fc22e497ca2e61718f0cfafae59c4d952
Alex <georgievalexandro@gmail.com>
2022-02-08 23:30:17 +0200

    job back to pipelineJob

--- jobs/job2.groovy
+++ jobs/job2.groovy
@@ -3,26 +3,28 @@ import groovy.json.JsonSlurper
 hudson.FilePath workspace = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
 def config = new JsonSlurper().parseText(new File("${workspace}/config.json").text)
 
-job("CHECKOUT") {
-    agent any 
-
-	triggers {
-		pollSCM('')
-	}
-
-	stages {
-		stage('checkout') {
-			steps {
-				echo "I am about to checkout this repo twice"
-				checkout scm: [
-					$class: 'GitSCM',
-					userRemoteConfigs: [
-						[url: 'https://github.com/georgievalexandro/learning-devops.git', credentialsId: '928429e8-5c06-4b6e-9f83-7a02081edc5e'],
-						[url: 'https://github.com/georgievalexandro/learning-devops.git', credentialsId: '1ce5941d-f076-4867-940b-f67f41ecc79c']
-					],
-		  			branches: [[name: 'refs/heads/master']]
-		  		], poll: true
-	  		}
-		}
-	}
+pipelineJob('CHECKOUT') {
+  definition {
+    cps {
+      script('''
+        pipeline {
+            agent any
+                stages {
+                    stage('Stage 1') {
+                        steps {
+                            echo 'logic'
+                        }
+                    }
+                    stage('Stage 2') {
+                        steps {
+                            echo 'logic'
+                        }
+                    }
+                }
+            }
+        }
+      '''.stripIndent())
+      sandbox()     
+    }
+  }
 }
