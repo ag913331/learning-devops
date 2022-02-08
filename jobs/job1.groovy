6d7207b618baa23cd78b95c07558022e6ebe4b08
Alex <georgievalexandro@gmail.com>
2022-02-08 14:20:21 +0200

    update

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -1,12 +1,13 @@
 // def repos = ["https://github.com/georgievalexandro/learning-devops.git", "https://github.com/georgievalexandro/nda.git"]
-// def repos = readFileFromWorkspace("")
+def repos = readFileFromWorkspace("seedA", "/jenkins_repos/github.txt")
+
 for(i in 0..2) {
     job("Repo${i}") {
         // scm {
         //     git(repos[i])
         // }
         steps {
-            shell("cat /jenkins_repos/github.txt")
+            shell("cat ${repos}")
         }
     }
 }
