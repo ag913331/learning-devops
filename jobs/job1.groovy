38de117287517dcf30ea6a73299a10996d0805a1
Alex <georgievalexandro@gmail.com>
2022-02-08 10:30:22 +0200

    test loop

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -1,23 +1,12 @@
-pipeline {
-	agent any 
+def repos = ["https://github.com/georgievalexandro/learning-devops.git", "https://github.com/georgievalexandro/nda.git"]
 
-	triggers {
-		pollSCM('')
-	}
-
-	stages {
-		stage('checkout') {
-			steps {
-				echo "I am about to checkout this repo twice"
-				checkout scm: [
-					$class: 'GitSCM',
-					userRemoteConfigs: [
-						[url: 'https://github.com/georgievalexandro/learning-devops.git', credentialsId: '928429e8-5c06-4b6e-9f83-7a02081edc5e'],
-						[url: 'https://github.com/georgievalexandro/learning-devops.git', credentialsId: '1ce5941d-f076-4867-940b-f67f41ecc79c']
-					],
-		  			branches: [[name: 'refs/heads/master']]
-		  		], poll: true
-	  		}
-		}
-	}
+for(i in 0..2) {
+    job("Scan repo-${i}") {
+        scm {
+            git(repos[i])
+        }
+        steps {
+            shell("Hello Jenkins ${i}")
+        }
+    }
 }
