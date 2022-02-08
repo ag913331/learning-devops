e1cfe363ded96c366429a32a3c1dfa70f351c82a
Alex <georgievalexandro@gmail.com>
2022-02-08 23:39:24 +0200

    brackets fixed

--- jobs/job2.groovy
+++ jobs/job2.groovy
@@ -3,22 +3,21 @@ import groovy.json.JsonSlurper
 hudson.FilePath workspace = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
 def config = new JsonSlurper().parseText(new File("${workspace}/config.json").text)
 
-pipelineJob('CHECKOUT') {
+pipelineJob('CHECKOUT2') {
   definition {
     cps {
       script('''
         pipeline {
             agent any
-                stages {
-                    stage('Stage 1') {
-                        steps {
-                            echo 'logic'
-                        }
+            stages {
+                stage('Stage 1') {
+                    steps {
+                        echo 'logic'
                     }
-                    stage('Stage 2') {
-                        steps {
-                            echo 'logic'
-                        }
+                }
+                stage('Stage 2') {
+                    steps {
+                        echo 'logic'
                     }
                 }
             }
