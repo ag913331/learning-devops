5d867b08747144c480336aea011f850e7b15bfef
Alex <georgievalexandro@gmail.com>
2022-02-09 09:38:00 +0200

    test

--- jobs/job2.groovy
+++ jobs/job2.groovy
@@ -1,35 +1,7 @@
-import groovy.json.JsonSlurper
-
-hudson.FilePath workspace = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
-def config = new JsonSlurper().parseText(new File("${workspace}/config.json").text)
-
 pipelineJob('PJ') {
   definition {
     cps {
-      script("
-        pipeline {
-            agent any
-            stages {
-                stage('Checkout stage') {
-                    ${config["repos"]}.eachWithIndex { repo, index -> 
-                        steps {
-                            echo '${repo["name"]}'
-                        }
-                    }
-                }
-                stage('Build') {
-                    steps {
-                        echo 'Building'
-                    }
-                }
-                stage('Test') {
-                    steps {
-                        echo 'Testing'
-                    }
-                }
-            }
-        }
-      ".stripIndent())
+      script(readFileFromWorkspace('Jenkinsfile'))
       sandbox()     
     }
   }
