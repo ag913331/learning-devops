3b6a0845d1f6ff3281121f4238be4e0f7b530de3
Alex <georgievalexandro@gmail.com>
2022-03-09 13:47:56 +0200

    test discard old build

--- maya/maya_build_dsl.groovy
+++ maya/maya_build_dsl.groovy
@@ -3,6 +3,11 @@ job('maya_build_dsl') {
     //     return shell()
     // }
     description("Testing maya_build via dsl")
+    publishers {
+        publishBuild {
+            discardOldBuilds(1, 2)
+        }
+    }
     steps {
         shell(readFileFromWorkspace('scripts/say_hello.py'))
     }
