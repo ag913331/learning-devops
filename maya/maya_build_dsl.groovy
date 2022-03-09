0c766f44616ac197e796916c2748c1777269ecd2
Alex <georgievalexandro@gmail.com>
2022-03-09 13:43:18 +0200

    add job description

--- maya/maya_build_dsl.groovy
+++ maya/maya_build_dsl.groovy
@@ -2,7 +2,7 @@ job('maya_build_dsl') {
     // def test_run_script() {
     //     return shell()
     // }
-
+    description("Testing maya_build via dsl")
     steps {
         shell(readFileFromWorkspace('scripts/say_hello.py'))
     }
