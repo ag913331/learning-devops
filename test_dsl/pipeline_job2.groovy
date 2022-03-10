4fa7bf182686c0a199dc000cc0554a7ef3bb0588
Alex <georgievalexandro@gmail.com>
2022-03-10 14:35:00 +0200

    test

--- test_dsl/pipeline_job2.groovy
+++ test_dsl/pipeline_job2.groovy
@@ -7,7 +7,10 @@ pipelineJob("anotherJob") {
         buildDiscarder { strategy { logRotator(1, 3, 1, 1) } }
         disableConcurrentBuilds()
         disableResume()
-        githubProjectProperty("https://github.com/ag913331/learning-devops", "learning-devops")
+        githubProjectProperty {
+            projectUrlStr("https://github.com/ag913331/learning-devops") 
+            displayName("learning-devops")
+        }
     }
 
     definition {
