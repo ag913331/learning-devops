ca4dbd26752acadfb7320d1423200216caa6da90
Alex <georgievalexandro@gmail.com>
2022-03-10 14:55:53 +0200

    test

--- test_dsl/pipeline_job2.groovy
+++ test_dsl/pipeline_job2.groovy
@@ -13,7 +13,7 @@ pipelineJob("anotherJob") {
         }
         durabilityHint { hint("PERFORMANCE_OPTIMIZED") } // Pipeline speed/durability override
         preserveStashes { buildCount(1) } // Preserve stashes from completed builds
-        upstream("learning-devops", "SUCCESS") // Build after other projects are built
+        upstream(projects="learning-devops", threshold="SUCCESS") // Build after other projects are built
     }
 
     definition {
