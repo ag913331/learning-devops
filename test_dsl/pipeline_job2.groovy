6151faad4aa4ba458dd5fba60c44d0c8e3753401
Alex <georgievalexandro@gmail.com>
2022-03-10 14:46:10 +0200

    test

--- test_dsl/pipeline_job2.groovy
+++ test_dsl/pipeline_job2.groovy
@@ -4,14 +4,15 @@ pipelineJob("anotherJob") {
         stringParam('name', "Ani", 'name of the person')
     }
     properties {
-        buildDiscarder { strategy { logRotator(1, 3, 1, 1) } }
-        disableConcurrentBuilds()
-        disableResume()
-        githubProjectProperty {
+        buildDiscarder { strategy { logRotator(1, 3, 1, 1) } } // Discard old builds
+        disableConcurrentBuilds { abortPrevious(true) } // Do not allow concurrent builds
+        disableResume() // Do not allow the pipeline to resume if the controller restarts
+        githubProjectProperty {  // GitHub project
             projectUrlStr("https://github.com/ag913331/learning-devops") 
             displayName("learning-devops")
         }
-        durabilityHint { hint("PERFORMANCE_OPTIMIZED") }
+        durabilityHint { hint("PERFORMANCE_OPTIMIZED") } // Pipeline speed/durability override
+        preserveStashes { buildCount(1) } // Preserve stashes from completed builds
     }
 
     definition {
