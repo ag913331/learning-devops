c4696823630f62229cafd02764e2e44d242b2fd2
Alex <georgievalexandro@gmail.com>
2022-03-10 13:41:30 +0200

    test

--- test_dsl/pipeline_job.groovy
+++ test_dsl/pipeline_job.groovy
@@ -10,12 +10,12 @@ pipelineJob("greetingJob") {
                 git {
                     branch('master')
                     remote {
-                        // credentials() // c7451d39-c220-4088-b7c0-4331ab39a2ba
+                        credentials('c7451d39-c220-4088-b7c0-4331ab39a2ba')
                         url('https://github.com/ag913331/learning-devops')
                     }
                 }
 
-                // scriptPath('test_dsl/pipeline_script.groovy')
+                scriptPath('test_dsl/pipeline_script.groovy')
             }
             // script('''
             //         pipeline {
