e9a3289aa479174c7253d80de77362f1dc59afd6
Alex <georgievalexandro@gmail.com>
2022-03-11 16:38:56 +0200

    job dsl testing

--- build/job_builder.groovy
+++ build/job_builder.groovy
@@ -1,11 +1,5 @@
-import jenkins.automation.builders.BaseJobBuilder
-
-new BaseJobBuilder(
-    name: "test job builder",
-    description: "Testing",
-    emails: ["foo@example.com", "bar@example.com"]
-).build(this).with {
-    steps {
-        shell("echo Hello Builder")
-    }
+def job = job('test_builder')
+job.with {
+    description 'A simple job'
+    sh 'Hello'
 }
