7ced9ddfe8210365aaa0d70023347c92e4dc20ca
Alex <georgievalexandro@gmail.com>
2022-03-11 09:25:28 +0200

    job dsl testing

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -1,16 +1,39 @@
-import groovy.json.JsonSlurper
-
-hudson.FilePath workspace = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
-def config = new JsonSlurper().parseText(new File("${workspace}/config.json").text)
-// def content = readFileFromWorkspace("seedA", "config.json")
-
-job("CHECKOUT") {
-    config["repos"].eachWithIndex { repo, index -> 
-        scm {
-            github(config["from"] + repo["name"], repo["branch"])
-        }
-        steps {
-            shell ("echo Checking")
-        }
+job("Dsl job") {
+    // authenticationToken('secret')
+    authorization {                 // Creates permission records. 
+        permissionAll('r3d')
     }
+    blockOn('testSeed')             // Block build if certain jobs are running. 
+    checkoutRetryCount(3)           // Sets the number of times the SCM checkout is retried on errors. 
+    compressBuildLog()              // Compresses the log file after build completion. 
+    concurrentBuild()               // Allows Jenkins to schedule and execute multiple builds concurrently.
+    displayName('DSL JOB')          // Sets the name to display instead of the actual name. 
+    steps {
+        shell("echo Testing dsl job")
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // config["repos"].eachWithIndex { repo, index -> 
+    //     scm {
+    //         github(config["from"] + repo["name"], repo["branch"])
+    //     }
+    //     steps {
+    //         shell ("echo Checking")
+    //     }
+    // }
 }
