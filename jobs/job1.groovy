3fca0c6e0164e01a1c44841039b04edb51cf35ed
Alex <georgievalexandro@gmail.com>
2022-03-11 14:57:15 +0200

    job dsl testing

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -18,7 +18,7 @@ job("Dsl_job") {
     steps {
         shell("echo Testing dsl job")
         // shell("printenv")
-        shell("echo ${file}")
+        shell("echo ${file['from']}")
     }
     
     
