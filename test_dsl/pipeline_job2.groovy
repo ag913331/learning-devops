2af1e409f204deb316dd6de84bf62e4fcb3f8387
Alex <georgievalexandro@gmail.com>
2022-03-14 16:25:59 +0200

    update

--- test_dsl/pipeline_job2.groovy
+++ test_dsl/pipeline_job2.groovy
@@ -1,82 +1,20 @@
-pipelineJob("greetingJob") {
-    description("Testing")
-    parameters {
-        stringParam('name', "Tom", 'name of the person')
-    }
-
-    definition {
-        cps {
-            script('''
-                pipeline {
-                    agent any                    
-                    stages {
-                        stage('Another Greeting') {
-                            steps {
-                                script {
-                                    echo "Hello!! ${name}"
-                                }
-                            }
-                        }
-                    }
-                }
-            ''')
-         }
-     }
-}
-
 pipelineJob("anotherJob") {
     displayName("Yet Another Job")
     description("Testing another job")
     
-    parameters {
-        stringParam('name', "Ani", 'name of the person')
-        globalVariableParam('GIT_VERSION', null, 'git version')
-        globalVariableParam('EXE_DIR', null, null)
-        globalVariableParam('DEBINFO_DIR', null, null)
-        globalVariableParam('DUPLICATE_BUILD', null, null)
-        globalVariableParam('SHOULD_BUILD', null, null)
-        globalVariableParam('phonon', null, null)
-        globalVariableParam('BUILD_TYPES', '${BUILD_TYPES}', null)
-    }
     properties {
-        buildDiscarder { strategy { logRotator(1, 3, 1, 1) } } // Discard old builds
-        disableConcurrentBuilds { abortPrevious(true) } // Do not allow concurrent builds
-        disableResume() // Do not allow the pipeline to resume if the controller restarts
-        githubProjectProperty {  // GitHub project
-            projectUrlStr("https://github.com/ag913331/learning-devops") 
-            displayName("learning-devops")
-        }
-        durabilityHint { hint("PERFORMANCE_OPTIMIZED") } // Pipeline speed/durability override
-        preserveStashes { buildCount(1) } // Preserve stashes from completed builds
-
-        pipelineTriggers {
-            triggers {
-                upstream { // Build after other projects are built
-                    upstreamProjects("greetingJob")
-                    threshold("SUCCESS")
-                }
-
-                cron { spec("H/15 * * * *") } // Build periodically
-
-                gitHubPushTrigger() // GitHub hook trigger for GITScm polling
-
-                pollSCM { // Poll SCM
-                    scmpoll_spec("H/12 * * * *")
-                    ignorePostCommitHooks(true)
-                } 
-                disabled()
-            }
-        }
+        disableConcurrentBuilds()
+        durabilityHint { hint("PERFORMANCE_OPTIMIZED") }
+        // pipelineTriggers { triggers { pollSCM { scmpoll_spec('*/1 * * * *') } } } }
+    parameters {
+        booleanParam('FORCE', false, 'Force build to overwrite existing files')
     }
-
-    environmentVariables {
-        env('nickname', 'Alex')
-        env('age', 25)
-    }
+    disabled()
 
     definition {
         cps {
             script(readFileFromWorkspace('test_dsl/workflow.groovy'))
-        }
+            sandbox()
+         }
      }
 }
