068134022e11a968fff4aa12ea424e920884613c
Alex <georgievalexandro@gmail.com>
2022-03-14 10:57:44 +0200

    update

--- test_dsl/pipeline_job.groovy
+++ test_dsl/pipeline_job.groovy
@@ -1,8 +1,10 @@
 @Grab('org.yaml:snakeyaml:1.17')
 import org.yaml.snakeyaml.Yaml
 
+def config = readFileFromWorkspace('config.yaml')
+
 Yaml parser = new Yaml()
-List example = parser.load(("../config.yaml" as File).text)
+List example = parser.load((config as File).text)
 println example
 
 pipelineJob("sss") {
