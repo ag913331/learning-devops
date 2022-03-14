364c400b2780fd0b3fcd5fa3b2570ae87fd916d7
Alex <georgievalexandro@gmail.com>
2022-03-14 10:04:35 +0200

    update

--- test_dsl/pipeline_job.groovy
+++ test_dsl/pipeline_job.groovy
@@ -1,22 +1,14 @@
-pipelineJob("greetingJob") {
+def config = readFileFromWorkspace('config.yaml')
+pipelineJob("sss") {
     description("Testing")
     parameters {
         stringParam('name', "Tom", 'name of the person')
     }
 
     definition {
-        cpsScm {
-            scm {
-                git {
-                    branch('master')
-                    remote {
-                        credentials('c7451d39-c220-4088-b7c0-4331ab39a2ba')
-                        url('https://github.com/ag913331/learning-devops')
-                    }
-                }
-
-                scriptPath('test_dsl/pipeline_script.groovy')
-            }
-         }
-     }
+        cps {
+            script(
+                echo "${config}"
+            )
+        }
 }
