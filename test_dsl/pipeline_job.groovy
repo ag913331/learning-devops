398c7636e13961398edbeef31eaaabe2673d7fc0
Alex <georgievalexandro@gmail.com>
2022-03-15 15:00:09 +0200

    test loop

--- test_dsl/pipeline_job.groovy
+++ test_dsl/pipeline_job.groovy
@@ -7,18 +7,17 @@ Yaml parser = new Yaml()
 def example_dict = parser.load(config)
 
 example_dict["pipelines"].each { p -> 
-    p.parameters.each { param -> 
-        println "$param.p_name"
-        // switch("${$param.p_type}") {
-        //     case "boolean":
-        //         booleanParam($param.p_name, $param.p_default, $param.p_description)
-        //     default:
-        //         break
-        // }
-    }
     pipelineJob("${p.name}") {
         description("${p.description}")
-
+        p.parameters.each { param -> 
+            println "$param.p_name"
+            // switch("${$param.p_type}") {
+            //     case "boolean":
+            //         booleanParam($param.p_name, $param.p_default, $param.p_description)
+            //     default:
+            //         break
+            // }
+        }
         // parameters {
         //     $p.parameters.each { param -> 
         //         println "$param"
