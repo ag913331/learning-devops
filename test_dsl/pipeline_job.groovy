1567367011bcdc072dd24c423e82ab40077ce2a0
Alex <georgievalexandro@gmail.com>
2022-03-15 15:01:05 +0200

    test loop

--- test_dsl/pipeline_job.groovy
+++ test_dsl/pipeline_job.groovy
@@ -9,26 +9,18 @@ def example_dict = parser.load(config)
 example_dict["pipelines"].each { p -> 
     pipelineJob("${p.name}") {
         description("${p.description}")
-        p.parameters.each { param -> 
-            println "$param.p_name"
-            // switch("${$param.p_type}") {
-            //     case "boolean":
-            //         booleanParam($param.p_name, $param.p_default, $param.p_description)
-            //     default:
-            //         break
-            // }
+
+        parameters {
+            p.parameters.each { param -> 
+                println "$param.p_name"
+                // switch("${$param.p_type}") {
+                //     case "boolean":
+                //         booleanParam($param.p_name, $param.p_default, $param.p_description)
+                //     default:
+                //         break
+                // }
+            }
         }
-        // parameters {
-        //     $p.parameters.each { param -> 
-        //         println "$param"
-        //         // switch("${$param.p_type}") {
-        //         //     case "boolean":
-        //         //         booleanParam($param.p_name, $param.p_default, $param.p_description)
-        //         //     default:
-        //         //         break
-        //         // }
-        //     }
-        // }
 
         disabled()
 
