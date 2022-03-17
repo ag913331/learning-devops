7240e1cb9bbc9da9a35aa90b0a29a9ce44bae087
Alex <georgievalexandro@gmail.com>
2022-03-17 10:53:36 +0200

    testing

--- test_jenkinsfile/job1/pipelineJob.groovy
+++ test_jenkinsfile/job1/pipelineJob.groovy
@@ -3,7 +3,7 @@ pipelineJob("pipeline_j1") {
 
     definition {
         cps {
-            script(readFileFromWorkspace('test_jenkinsfile/Jenkinsfile'))
+            script(readFileFromWorkspace('test_jenkinsfile/job1/Jenkinsfile'))
             sandbox()
         }
     }
