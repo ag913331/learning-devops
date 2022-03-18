3db9bf0a1b181364d9e798600213166acca18910
Alex <georgievalexandro@gmail.com>
2022-03-18 16:01:16 +0200

    testing utils

--- jenkins_jobs/jobs/dsl.groovy
+++ jenkins_jobs/jobs/dsl.groovy
@@ -9,22 +9,22 @@ class DefUtils {
     }
 }
 
-Job job = pipelineJob('dsl-job-one_withUtils') {
+Job job = job('dsl-job-one_withUtils') {
     // description("dsl-job-one implementation from code with utils")
 
     disabled()
 
-    environmentVariables {
-        env('ONE', '1')
-        env('TWO', '2')
-    }
+    // environmentVariables {
+    //     env('ONE', '1')
+    //     env('TWO', '2')
+    // }
 
-    definition {
-        cps {
-            script(readFileFromWorkspace('jenkins_jobs/jobs/dsl.groovy'))
-            sandbox()
-        }
-    }
+    // definition {
+    //     cps {
+    //         script(readFileFromWorkspace('jenkins_jobs/jobs/dsl.groovy'))
+    //         sandbox()
+    //     }
+    // }
 }
 
 DefUtils.setupDefinition(job, "dsl-job-one implementation from code with utils")
