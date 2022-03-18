6392a5ea279d8a93190fd7af91c2beb6663d3ca3
Alex <georgievalexandro@gmail.com>
2022-03-18 14:07:17 +0200

    testing utils

---
+++ jenkins_jobs/src/jobs/pJ1/dsl.groovy
@@ -0,0 +1,14 @@
+import jenkins_jobs.src.jobs.pJ1.utils.DefUtils
+
+def job = pipelineJob('dsl-job-one_withUtils') {
+    description("dsl-job-one implementation from code with utils")
+
+    disabled()
+
+    environmentVariables {
+        env('ONE', '1')
+        env('TWO', '2')
+    }
+}
+
+DefUtils.setupDefinition(job, 'jenkins_jobs/src/jobs/pj1/dsl.groovy')
---
+++ jenkins_jobs/src/jobs/pJ1/utils/DefUtil.groovy
@@ -0,0 +1,16 @@
+package utils
+
+// import javaposse.jobdsl.dsl.Job
+
+class DefUtils {
+    static void setupDefinition(def job, String workflowPath) {
+        job.with {
+            definition {
+                cps {
+                    script(readFileFromWorkspace(workflowPath))
+                    sandbox()
+                }
+            }
+        }
+    }
+}
