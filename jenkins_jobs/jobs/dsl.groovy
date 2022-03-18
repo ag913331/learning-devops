5af0a4dc4d80925c5954871b63b669b64744edfa
Alex <georgievalexandro@gmail.com>
2022-03-18 16:16:14 +0200

    testing utils

--- jenkins_jobs/jobs/dsl.groovy
+++ jenkins_jobs/jobs/dsl.groovy
@@ -1,25 +1,23 @@
 import javaposse.jobdsl.dsl.DslFactory
 import javaposse.jobdsl.dsl.Job
 class DefUtils {
-    static void setupDefinition(Job job, String workflowPath) {
+    static void setupDefinition(Job job) {
         job.with {
             // description(desc)
             definition {
                 cps {
-                    script(readFileFromWorkspace(workflowPath))
+                    script(readFileFromWorkspace('jenkins_jobs/jobs/workflow.groovy'))
                     sandbox()
                 }
             }
-
-            disabled()
         }
     }
 }
 
 Job job = pipelineJob('dsl-job-one_withUtils') {
-    description("dsl-job-one implementation from code with utils")
+    description("dsl-job-one implementation from code with utils vv")
 
-    
+    disabled()
 
     // environmentVariables {
     //     env('ONE', '1')
@@ -29,4 +27,4 @@ Job job = pipelineJob('dsl-job-one_withUtils') {
     
 }
 
-DefUtils.setupDefinition(job, 'jenkins_jobs/jobs/workflow.groovy')
+DefUtils.setupDefinition(job)
