bc54849aa0f77735ded28ce799a6831f53ad53f6
Alex <georgievalexandro@gmail.com>
2022-03-21 09:44:11 +0200

    update

--- jenkins_jobs/src/jobs/dsl.groovy
+++ jenkins_jobs/src/jobs/dsl.groovy
@@ -1,7 +1,7 @@
 import javaposse.jobdsl.dsl.DslFactory
 import javaposse.jobdsl.dsl.Job
 
-import util.DefUtils
+import jenkins_jobs.src.jobs.util.DefUtils
 // class DefUtils {
 //     static void setupDefinition(Job job, String desc) {
 //         job.with {
